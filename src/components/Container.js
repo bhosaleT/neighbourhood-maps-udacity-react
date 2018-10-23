2eb6ac6fceed30a2e4b24ae86590b90d6306e3a2
Tejas Bhosale <bhosaletejas93@gmail..com>
2018-10-23 13:56:09 +0530

    adding the google map

--- src/components/Container.js
+++ src/components/Container.js
@@ -1,14 +1,26 @@
 import React from "react";
 import Header from "./Header";
+import {GoogleApiWrapper} from 'google-maps-react';
+import {Map} from './Map';
 import "../CSS/App.scss";
 
 export class Container extends React.Component {
   render() {
+      const style= {
+          width: '100vw',
+          height: '100vh'
+      }
     return (
       <div>
         <Header />
-        this is the map container the map goes here.
+        <div style={style}>
+        <Map google={this.props.google}/>
+        </div>
       </div>
     );
   }
 }
+
+export default GoogleApiWrapper({
+    apiKey: 'AIzaSyCgByDyHejXcnEYiAohk2kMYN0npMXsJTc'
+})(Container)
